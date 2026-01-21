19aa626431849e2a5aeab5d6675b4abd451235e5
Sujeeth-Varma <sujeethvarma27@gmail.com>
2026-01-21 11:58:42 +0530

    add index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+// console.log("Hello world");
+// let data = 0;
+// // console.log(++data);
+
+// let str1 = "hello";
+// let str2 = "hello";
+
+// // console.log(str1 === str2);
+
+// let num1 = 1;
+// let num2 = "1";
+// // console.log(num1 === num2);
+
+// // Logical Operators
+// console.log(!false);
+
+// // for (let i = 0; i <= 10; i++) {
+// //   console.log(i);
+// // }
+
+// const arr = ["A", "B", "C", "D", "E"];
+
+// // for (let i = 0; i < arr.length; i++) {
+// //   console.log(arr[i]);
+// // }
+
+// // arr.map((letter) => {
+// //   console.log(letter);
+// // });
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// nums.map((num) => {
+//   console.log(num * num);
+// });
+
+// for (let i = 0; i < nums.length; i++) {
+//   console.log(nums[i] * nums[i]);
+// }
+
+const data = {
+  name: "username",
+  email: "random@gmail.com",
+  phone: [1234, 2453],
+  address: {
+    city: "Hyd",
+    state: "TS",
+  },
+};
+
+// console.log(data);
+console.log(data.name);
+console.log(data.address.city);
